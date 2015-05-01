ae3c043b9eef7320b6f39c6e77efad8769cf7307
Philipp S. aka. ErdnussFlipS <erdnussflips93@gmail.com>
2015-05-01 14:37:44 +0200

    FirewallHelper improved. First GUI Window for test implemented

--- WindowsFirewallDashboard/MainWindow.xaml.cs
+++ WindowsFirewallDashboard/MainWindow.xaml.cs
@@ -12,17 +12,82 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsFirewallHelper;
+using MahApps.Metro.Controls;
 
 namespace WindowsFirewallDashboard
 {
 	/// <summary>
 	/// Interaktionslogik für MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : MetroWindow
 	{
+		private Win8Controller FwController = new Win8Controller();
 		public MainWindow()
 		{
 			InitializeComponent();
+			InitializeEvents();
+		}
+
+		public void InitializeEvents()
+		{
+			Loaded += MainWindow_Loaded;
+			btnEnable.Click += BtnEnableOnClick;
+			btnEnableDomain.Click += BtnEnableDomainOnClick;
+			btnEnablePrivate.Click += BtnEnablePrivateOnClick;
+			btnEnablePublic.Click += BtnEnablePublicOnClick;
+			btnDisable.Click += BtnDisableOnClick;
+			btnDisableDomain.Click += BtnDisableDomainOnClick;
+			btnDisablePrivate.Click += BtnDisablePrivateOnClick;
+			btnDisablePublic.Click += BtnDisablePublicOnClick;
+		}
+
+		private void BtnDisablePublicOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.DisableFirewallOnPublicNetworks();
+		}
+
+		private void BtnDisablePrivateOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.DisableFirewallOnPrivateNetworks();
+		}
+
+		private void BtnDisableDomainOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.DisableFirewallOnDomainNetworks();
+		}
+
+		private void BtnDisableOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.DisableFirewall();
+		}
+
+		void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (!Win8Controller.HasAdministratorPrivileges())
+			{
+				MessageBox.Show("You must have admin privileges to change firewall settings!");
+			}
+		}
+
+		private void BtnEnableOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.EnableFirewall();
+		}
+
+		private void BtnEnablePrivateOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.EnableFirewallOnPrivateNetworks();
+		}
+
+		private void BtnEnableDomainOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.EnableFirewallOnDomainNetworks();
+		}
+
+		private void BtnEnablePublicOnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			FwController.EnableFirewallOnPublicNetworks();
 		}
 	}
 }
--- WindowsFirewallHelper/WinXPController.cs
+++ WindowsFirewallHelper/WinXPController.cs
@@ -3,63 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 using NetFwTypeLib;
 
 namespace WindowsFirewallHelper
 {
-	public enum FirewallStatus
+	public class WinXPController : AController
 	{
-		Enabled, Disabled
-	}
+		private INetFwMgr firewallManager;
 
-    public class Controller
-    {
-		[DllImport("shell32.dll", EntryPoint = "IsUserAnAdmin")]
-		public static extern bool IsUserAnAdministrator();
-
-		private static Object getFwObject(String typeName)
+		public INetFwMgr FirewallManager
 		{
-			Type type;
-
-			switch (typeName)
+			get
 			{
-				case "INetFwMgr":
-				{
-					type = Type.GetTypeFromCLSID(new Guid("{304CE942-6E39-40D8-943A-B913C40C9CD4}"));
-					break;
-				}
-				case "INetAuthApp":
-				{
-					type = Type.GetTypeFromCLSID(new Guid("{EC9846B3-2762-4A6B-A214-6ACB603462D2}"));
-					break;
-				}
-				case "INetOpenPort":
-				{
-					type = Type.GetTypeFromCLSID(new Guid("{0CA545C6-37AD-4A6C-BF92-9F7610067EF5}"));
-					break;
-				}
-				default:
-					return null;
+				if (firewallManager != null) return firewallManager;
+				firewallManager = (INetFwMgr)getFwObject("INetFwMgr");
+				return firewallManager;
 			}
-
-			return Activator.CreateInstance(type);
 		}
-
-	    private INetFwMgr firewallManager;
-
-	    public INetFwMgr FirewallManager
-	    {
-		    get
-		    {
-			    if (firewallManager != null) return firewallManager;
-
-			    //var type = Type.GetTypeFromProgID("HNetCfg.FwMgr", false);
-			    firewallManager = (INetFwMgr)getFwObject("INetFwMgr");
-
-			    return firewallManager;
-		    }
-	    }
 
 		protected INetFwOpenPort GetNewOpenPortInstance()
 		{
@@ -69,35 +29,35 @@ namespace WindowsFirewallHelper
 		protected INetFwAuthorizedApplication GetNewAuthorizedApplicationInstance()
 		{
 			return (INetFwAuthorizedApplication)getFwObject("INetAuthApp");
-		} 
+		}
 
-	    public FirewallStatus GetFirewallStatus()
-	    {
+		public override FirewallStatus GetFirewallStatus()
+		{
 			var firewallEnabled = FirewallManager.LocalPolicy.CurrentProfile.FirewallEnabled;
-		    return firewallEnabled ? FirewallStatus.Enabled : FirewallStatus.Disabled;
-	    }
+			return firewallEnabled ? FirewallStatus.Enabled : FirewallStatus.Disabled;
+		}
 
-	    public Boolean EnableFirewall()
-	    {
-		    try
-		    {
+		public override Boolean EnableFirewall()
+		{
+			try
+			{
 				firewallManager.LocalPolicy.CurrentProfile.FirewallEnabled = true;
-		    }
-		    catch (Exception)
-		    {
-			    return false;
-		    }
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
 
-		    return true;
-	    }
+			return true;
+		}
 
-		public Boolean DisableFirewall()
+		public override Boolean DisableFirewall()
 		{
 			try
 			{
 				firewallManager.LocalPolicy.CurrentProfile.FirewallEnabled = false;
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
 				return false;
 			}
@@ -106,32 +66,32 @@ namespace WindowsFirewallHelper
 		}
 
 		public List<INetFwOpenPort> GetCurrentGlobalAuthorizedOpenPorts()
-	    {
-		    var openPorts = new List<INetFwOpenPort>();
-		    var ports = FirewallManager.LocalPolicy.CurrentProfile.GloballyOpenPorts;
+		{
+			var openPorts = new List<INetFwOpenPort>();
+			var ports = FirewallManager.LocalPolicy.CurrentProfile.GloballyOpenPorts;
 			var enumerate = ports.GetEnumerator();
 
-			while (enumerate.MoveNext()) 
+			while (enumerate.MoveNext())
 			{
 				openPorts.Add((INetFwOpenPort)enumerate.Current);
 			}
 
 			return openPorts;
-	    }
+		}
 
-	    public List<INetFwAuthorizedApplication> GetCurrentGlobalAuthorizedApplications()
-	    {
-		    var authorizedApplications = new List<INetFwAuthorizedApplication>();
+		public List<INetFwAuthorizedApplication> GetCurrentGlobalAuthorizedApplications()
+		{
+			var authorizedApplications = new List<INetFwAuthorizedApplication>();
 			var applications = FirewallManager.LocalPolicy.CurrentProfile.AuthorizedApplications;
-		    var enumerate = applications.GetEnumerator();
+			var enumerate = applications.GetEnumerator();
 
 			while (enumerate.MoveNext())
 			{
 				authorizedApplications.Add((INetFwAuthorizedApplication)enumerate.Current);
 			}
 
-		    return authorizedApplications;
-	    }
+			return authorizedApplications;
+		}
 
 		public void AddAuthorizedPort(
 			string applicationName,
@@ -141,7 +101,7 @@ namespace WindowsFirewallHelper
 			string remoteAddresses = null,
 			NET_FW_SCOPE_ scope = NET_FW_SCOPE_.NET_FW_SCOPE_ALL
 		)
-		{ 
+		{
 			var ports = FirewallManager.LocalPolicy.CurrentProfile.GloballyOpenPorts;
 
 			var port = GetNewOpenPortInstance();
@@ -169,7 +129,7 @@ namespace WindowsFirewallHelper
 			var application = GetNewAuthorizedApplicationInstance();
 			application.Name = applicationName; /*set the name of the application */
 			application.ProcessImageFileName = applicationProcessFileName; /* set this property to the location of   the executable file of the application*/
-			application.Enabled =  true; //enable it
+			application.Enabled = true; //enable it
 			application.IpVersion = ipVersion;
 			application.RemoteAddresses = remoteAddresses;
 			application.Scope = scope;
@@ -177,5 +137,5 @@ namespace WindowsFirewallHelper
 			/*now add this application to AuthorizedApplications collection */
 			applications.Add(application);
 		} 
-    }
+	}
 }
