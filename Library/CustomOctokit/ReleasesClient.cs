446b06afdfa1be2987d2ed50a821ee046042dbbe
Philipp S. aka. ErdnussFlipS <erdnussflips93@gmail.com>
2016-05-02 23:24:20 +0200

    Squashed 'GitHubUpdateManger.NET/' content from commit 9d19386
    git-subtree-dir: GitHubUpdateManger.NET
    git-subtree-split: 9d19386fc0618c40ac8ef8080c1f99864a23917b

---
+++ Library/CustomOctokit/ReleasesClient.cs
@@ -0,0 +1,27 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUpdateManger.Library.CustomOctokit
+{
+	class ReleasesClient : Octokit.ReleasesClient
+	{
+		public ReleasesClient(IApiConnection con) : base(con) { }
+
+
+		public Task<IReadOnlyList<Release>> GetPaginated(string owner, string name, int page, int perPage)
+		{
+			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+			Ensure.ArgumentNotNullOrEmptyString(name, "repository name");
+
+			var endpoint = ApiUrls.Releases(owner, name);
+			var parameter = new Dictionary<string, string>();
+			parameter.Add("page", page.ToString());
+			parameter.Add("per_page", perPage.ToString());
+			return ApiConnection.GetAll<Release>(endpoint, parameter, "application/vnd.github.v3");
+		}
+	}
+}
